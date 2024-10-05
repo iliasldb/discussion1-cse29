691a2d23415f371fb39f51d23c608dfc72b28c2d
iliasldb <116680914+iliasldb@users.noreply.github.com>
2024-10-05 17:34:27 +0000

    playing with inspect  code from lecture

---
+++ inspect.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void inspect(char s[]) {
+    int index = 0;
+    printf("%s, length %ld: ", s, strlen(s));
+    while(s[index] != 0) {
+        char current = s[index];
+        //print the decimal number representing the char at index, then it's binary representation bit by bit
+        printf("%c (%hhu 0b", current, current);
+        for(int place = 128; place > 0; place /= 2) {
+            if((current & place) == 0) printf("0");
+            else printf("1");
+        }
+        printf(") ");
+        index += 1;
+    }
+    printf("\n");
+}
+
+int main() {
+    char input[100];
+    fgets(input, 100, stdin);
+    printf("\n");
+    inspect(input);
+}
